8c6626c615aff7374340b66e4bc860ce94e99ba8
SONJAY-DEY <sonjay7654@gmail.com>
2024-09-26 15:11:40 +0600

    Add files via upload

---
+++ New folder/js/bank.js
@@ -0,0 +1,6 @@
+document.getElementById('longin-submit').addEventListener('click', function(){
+    console.log(' button');
+    const emailField= document.getElementById("user-email");
+    const remal=emailField.value;
+    console.log(remal);
+})
